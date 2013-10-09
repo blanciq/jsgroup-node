6f9f2a986184afef5c2b9e98e32ca540e6021677
Sławomir Zarucki <zarucki@gmail.com>
2013-10-09 16:55:53 +0200

    This should work not only on 'My machine' ;)

--- jsonResponse.js
+++ jsonResponse.js
@@ -1,5 +1,5 @@
 module.exports = function (response, request, data) {
-    response.writeHead(200, {"Access-Control-Allow-Origin": "http://localhost:8889"});
+    response.writeHead(200, {"Access-Control-Allow-Origin": "*"});
     response.write(JSON.stringify(data));
     response.end();
 }
